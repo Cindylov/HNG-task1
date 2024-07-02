d1b0db7456be9582788cca711612440969820a92
Cindylov <cynthiaujunwa4@gmail.com>
2024-07-02 11:41:54 +0100

    task

--- javascript/personal.js
+++ javascript/personal.js
@@ -1,21 +1,9 @@
-// function to display the current time
-function updateTime(){
-  const timeElement = document.getElementById('utc-time');
+const updateTime = () => {
   const now = new Date();
-  timeElement.textContent = now.toLocaleTimeString('en-US', {hour12: false});
-}
+  const timeUTC = now.toISOString().substring(11, 19);
+  document.querySelector('[data-testid="currentTimeUTC"]').textContent = ` ${timeUTC}`;
+  document.querySelector('[data-testid="currentDay"]').textContent = ` ${now.toLocaleDateString('en-US',{ weekday: 'long'})}`;
+};
 
-// function to display the current day of week
-function updateDay() {
-  const dayElement = document.getElementById('current-day');
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const now = new Date();
-  dayElement.textContent = days[now.getDay()];
-}
-
-// update time and day immediately
 updateTime();
-updateDay();
-
-// update time every second
-setInterval(updateTime, 1000)
+setInterval(updateTime, 1000);
